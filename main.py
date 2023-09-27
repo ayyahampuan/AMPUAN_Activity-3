47e5999f3732e17d4cf7a60a2c01b981516c7cf5
ayyahampuan <144468270+ayyahampuan@users.noreply.github.com>
2023-09-27 04:11:25 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+Purchase_amount = float(input("Enter purchase amount:"))
+
+calculate = Purchase_amount * 0.06
+
+print(Purchase_amount)
+
+print("Enter purchase amount:", (calculate))
+print("Sales Tax:", (calculate))
